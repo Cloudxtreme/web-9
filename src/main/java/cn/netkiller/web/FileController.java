98849c02d58e6131d1d1a2432ad7d81da29f5cad
skyline <skyline.chen@icloud.com>
2016-10-09 11:13:32 +0800

    test

--- src/main/java/cn/netkiller/web/FileController.java
+++ src/main/java/cn/netkiller/web/FileController.java
@@ -1,7 +1,10 @@
 package cn.netkiller.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/file")
@@ -9,6 +12,18 @@ public class FileController {
 
 	public FileController() {
 		// TODO Auto-generated constructor stub
+	}
+	
+	@Autowired
+	private StringRedisTemplate stringRedisTemplate;
+	
+	@RequestMapping("/test")
+	@ResponseBody
+	public String ping() {
+		String message = "PONG";
+		stringRedisTemplate.opsForValue().set("hello", "world");
+		message = stringRedisTemplate.opsForValue().get("hello");
+		return message;
 	}
 
 }
